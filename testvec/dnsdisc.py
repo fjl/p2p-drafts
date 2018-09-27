c81fcbf4decf6c2f67019fa914f10edaf084d578
Felix Lange <fjl@twurst.com>
2018-09-27 17:23:03 +0200

    testvec: more work on DNS discovery

--- testvec/dnsdisc.py
+++ testvec/dnsdisc.py
@@ -1,48 +1,200 @@
 # -*- coding: utf-8 -*-
 
-import enr
-import rlp
 import base64
+import dns.resolver
+import re
+import rlp
 import sha3
+from collections import OrderedDict
+from enr import ENR
 
-def make_enr_tree(enrs, links, seq, privkey):
-    leaves = list(map(enr_entry, enrs)) + list(map(link_entry, links))
-    rhash, tree = subtree_entry([h for (h, _) in leaves])
-    root = root_entry(rhash, seq, sign_root(privkey, rhash, seq))
-    records = [("@", root), (hash_to_subdomain(rhash), tree)]
-    return records + [(hash_to_subdomain(h), e) for (h, e) in leaves]
+# Resolvers
 
-def to_zonefile(records):
-    return "\n".join([
-        "{:20}   IN    TXT   \"{}\"".format(h, e)
-        for (h, e) in records
-    ])
+class SystemResolver():
+    def resolveTXT(self, name):
+        answers = dns.resolver.query(name, 'TXT')
+        return [''.join(rdata.strings) for rdata in answers]
 
-def enr_entry(enr):
-    enc = enr.encode()
-    txt = "enr=" + base64.b85encode(enc).decode()
-    return (hash(enc), txt)
+# The Tree
 
-def link_entry(link):
-    txt = "enr-tree-link=" + link
-    return (hash(txt), txt)
+class Tree():
+    def __init__(self, enrs, links, seq):
+        leaves = list(map(enrEntry, enrs)) + list(map(linkEntry, links))
+        entries = self._build(leaves)
+        self.entries = {e.subdomain(): e for e in entries + leaves}
+        roothash = entries[0].subdomain()
+        self.root = rootEntry(roothash, seq, None)
 
-def subtree_entry(hashes):
-    txt = "enr-tree=" + ",".join([hash_to_subdomain(h) for h in hashes])
-    return (hash(txt), txt)
+    @classmethod
+    def with_root(cls, root):
+        tree = cls.__new__(cls)
+        tree.root = root
+        tree.entries = {}
+        return tree
 
-def root_entry(rhash, seq, sig):
-    sig = base64.b85encode(sig).decode()
-    return "enr-tree-root=v1 hash={} seq={} sig={}".format(hash_to_subdomain(rhash), seq, sig)
+    def _build(self, entries):
+        if len(entries) <= _MAX_INTERMEDIATE_HASHES:
+            return [subtreeEntry([e.subdomain() for e in entries])]
+        else:
+            roots, all = [], []
+            while len(entries) > 0:
+                children = self._build(entries[:_MAX_INTERMEDIATE_HASHES])
+                roots.append(children[0])
+                all.extend(children)
+                entries = entries[_MAX_INTERMEDIATE_HASHES:]
+            return self._build(roots) + all
 
-def sign_root(privkey, rhash, seq):
-    sighash = sha3.keccak_256((rhash.hex() + str(seq)).encode()).digest()
-    return privkey.sign_recoverable(sighash, hasher=None)
+    def sign(self, privkey):
+        self.root.sign(privkey)
+        return self
 
-def hash_to_subdomain(hash):
-    return hash[:8].hex()
+    @classmethod
+    def resolve(cls, name, resolver=SystemResolver()):
+        e = _resolveEntry(resolver, name)
+        if isinstance(e, rootEntry):
+            tree = cls.with_root(e)
+            tree.resolve_updates(name, resolver)
+            return tree
+        else:
+            raise 'no tree found'
 
-def hash(entry):
-    if isinstance(entry, str):
-        entry = entry.encode()
-    return sha3.keccak_256(entry).digest()
+    def resolve_updates(self, name, resolver=SystemResolver()):
+        want = OrderedDict()
+        want[self.root.roothash] = True
+        new_entries = {}
+        while len(want) > 0:
+            h, _ = want.popitem(False)
+            if h in self.entries:
+                new_entries[h] = e
+                continue
+            # need this entry, resolve
+            e = _resolveEntry(resolver, h + '.' + name)
+            if isinstance(e, subtreeEntry):
+                new_entries[h] = e
+                for d in e.subdomains:
+                    want[d] = True
+            elif isinstance(e, enrEntry) or isinstance(e, linkEntry):
+                new_entries[h] = e
+        # done, set new entries
+        self.entries = new_entries
+
+    def to_zonefile(self):
+        rr = ['{:20}   60      IN    TXT   "{}"'.format('@', self.root.text())]
+        rc = ['{:20}   86900   IN    TXT   "{}"'.format(e.subdomain(), e.text()) for e in self.entries.values()]
+        return '\n'.join(rr + rc)
+
+def _resolveEntry(resolver, name):
+    for txt in resolver.resolveTXT(name):
+        e = parse_entry(txt)
+        if e is not None:
+            return e
+    raise 'no entry found at ' + name
+
+# Tree Entries
+
+_HASH_ABBREV = 8
+_MAX_INTERMEDIATE_HASHES = round(300 / (_HASH_ABBREV*2))
+
+class entry():
+    def hash(self):
+        return sha3.keccak_256(self.text().encode()).digest()
+
+    def subdomain(self):
+        return self.hash()[:_HASH_ABBREV].hex()
+
+class enrEntry(entry):
+    prefix = 'enr='
+
+    def __init__(self, enr):
+        self.enr = enr
+
+    def text(self):
+        enc = self.enr.encode()
+        return enrEntry.prefix + base64.b85encode(enc).decode()
+
+    @classmethod
+    def parse(cls, txt):
+        raw = base64.b85decode(txt[len(cls.prefix):])
+        return cls(ENR.from_rlp(raw))
+
+class linkEntry(entry):
+    prefix = 'enr-tree-link='
+
+    def __init__(self, link):
+        self.link = link
+
+    def text(self):
+        return linkEntry.prefix + self.link
+
+    @classmethod
+    def parse(cls, txt):
+        return cls(txt[len(cls.prefix):])
+
+class subtreeEntry(entry):
+    prefix = 'enr-tree='
+
+    def __init__(self, subdomains):
+        self.subdomains = subdomains
+
+    def text(self):
+        return subtreeEntry.prefix + ','.join(self.subdomains)
+
+    @classmethod
+    def parse(cls, txt):
+        return cls(txt[len(cls.prefix):].split(','))
+
+class rootEntry(entry):
+    prefix = 'enr-tree-root=v1'
+
+    def __init__(self, roothash, seq, sig):
+        self.seq = seq
+        self.roothash = roothash
+        self.sig = sig
+
+    def sign(self, privkey):
+        sighash = sha3.keccak_256(self.signed_text().encode()).digest()
+        self.sig = privkey.sign_recoverable(sighash, hasher=None)
+
+    def subdomain(self):
+        return ''
+
+    def text(self):
+        if self.sig is None:
+            raise RuntimeError('tree is not signed')
+        sig = base64.b85encode(self.sig).decode()
+        return self.signed_text() + ' sig=' + sig
+
+    def signed_text(self):
+        return '{} hash={} seq={}'.format(rootEntry.prefix, self.roothash, self.seq)
+
+    pattern = re.compile(re.escape(prefix) + ' hash=([0-9a-fA-F]{16}) seq=([0-9]+) sig=(.+)')
+
+    @classmethod
+    def parse(cls, txt):
+        m = cls.pattern.match(txt)
+        try:
+            roothash = m.group(1)
+            seq = int(m.group(2))
+            sig = base64.b85decode(m.group(3))
+        except Exception:
+            raise ParseError('invalid tree root ' + txt)
+        if len(roothash) != 16:
+            raise ParseError('invalid root hash length')
+        if len(sig) != 65:
+            raise ParseError('invalid signature length')
+        return cls(roothash, seq, sig)
+
+def parse_entry(txt):
+    if txt.startswith(rootEntry.prefix):
+        return rootEntry.parse(txt)
+    elif txt.startswith(subtreeEntry.prefix):
+        return subtreeEntry.parse(txt)
+    elif txt.startswith(enrEntry.prefix):
+        return enrEntry.parse(txt)
+    elif txt.startswith(linkEntry.prefix):
+        return linkEntry.parse(txt)
+    else:
+        return None
+
+class ParseError(BaseException):
+    pass
--- testvec/test-dnsdisc.py
+++ testvec/test-dnsdisc.py
@@ -2,24 +2,55 @@
 
 import coincurve
 import dnsdisc
+import dns.resolver
 from enr import ENR
 
-def test_enr_tree_example():
-    keys = [
-        coincurve.PrivateKey.from_hex('b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291'),
-        coincurve.PrivateKey.from_hex("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a"),
-        coincurve.PrivateKey.from_hex("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee"),
-        coincurve.PrivateKey.from_hex("45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"),
-    ]
-    enrs = [
-        ENR().set("ip", "203.0.113.1").sign(keys[0]),
-        ENR().set("ip", "198.51.100.99").sign(keys[1]),
-    ]
-    links = [
-        "morenodes.example.org",
-    ]
-    tree = dnsdisc.make_enr_tree(enrs, links, 3, keys[2])
+testkeys = [
+    coincurve.PrivateKey.from_hex('b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291'),
+    coincurve.PrivateKey.from_hex('8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a'),
+    coincurve.PrivateKey.from_hex('49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee'),
+    coincurve.PrivateKey.from_hex('45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8'),
+]
 
-    print("Example zone file:\n")
-    print(dnsdisc.to_zonefile(tree))
-    print("\n\n")
+def test_tree_example():
+    enrs = [
+        ENR().set('ip', '203.0.113.1').sign(testkeys[0]),
+        ENR().set('ip', '198.51.100.99').sign(testkeys[1]),
+    ]
+    links = ['morenodes.example.org',]
+    tree = dnsdisc.Tree(enrs, links, 3).sign(testkeys[2])
+    print('Example zone file:\n')
+    print(tree.to_zonefile())
+    print('\n\n')
+
+
+def test_tree_resolve():
+    ns = DictResolver('nodes.example.org', {
+        '':                 'enr-tree-root=v1 hash=797a23d8100f7078 seq=3 sig=x<MqCoo*q-S?0wcg^F6}d^{)Y+8}zKrb=T*3)wWDA&{%R5&!lrGTKo@c*@~@Yoo&#+>bM^)@Y(v5}hf<00',
+        '797a23d8100f7078': 'enr-tree=088bef4b06632cdb,eb9c1ad35aac9c71,5b378d39913b1f93',
+        '088bef4b06632cdb': 'enr=_<guQjUTCP{RqHWjGNW?LRl*ySR#tAhp%LQL3m%l>?Y;TWG<eXes@Z`*$>0`ztySi++YoiEfGsM%?UAj#)fmH0fK2{f_5~5X>f$g0C53{b7f<2GBq}9F`)y>V@$3MNvyO1*rdj`{)|<(4G~-P0Ct?gKo2rl%`rF',
+        'eb9c1ad35aac9c71': 'enr=_<guQI<5l3<MLpP%$R(Ut$~G6VBUTe_2<DP7@AynC@+0)M3P>A=|HjLA-MoVR+B&Rn&O{CUb#id6R~XaJiQj&0fK2{f_5~5X>f$bGh}0lb7f<2GBq}9F`)wOd@Tj%lD4x~phg<p>jjtP)0mE_TKMaV2tv>IW4Dk',
+        '5b378d39913b1f93': 'enr-tree-link=morenodes.example.org',
+    })
+    tree = dnsdisc.Tree.resolve('nodes.example.org', ns)
+    assert(len(tree.entries) == 4)
+
+def test_enr_tree_big():
+    enrs = [ENR().set('i', str(i).encode()).sign(testkeys[0]) for i in range(0, 500)]
+    tree = dnsdisc.Tree(enrs, [], 3).sign(testkeys[2])
+
+
+class DictResolver():
+    def __init__(self, domain, d):
+        self.d = {}
+        for (k, v) in d.items():
+            if k == '':
+                k = domain
+            else:
+                k = k + '.' + domain
+            self.d[k] = v
+
+    def resolveTXT(self, name):
+        if name not in self.d:
+            raise dns.resolver.NXDOMAIN
+        return [self.d[name]]
